7bc4cd4842b4c065ec6fb6870f5b6ba93cb61e87
Gavin Riggi <griggi@clarkson.edu>
2022-07-07 17:46:36 -0400

    change service management

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,12 +2,10 @@ class cas::service {
   assert_private()
 
   if $cas::service_manage {
-    systemd::dropin_file { "${cas::service_name}.conf":
-      unit    => "${cas::service_name}.service",
+    systemd::unit_file { "${cas::service_name}.service":
       content => epp("${module_name}/cas.service.conf.epp"),
-    } ~> service { $cas::service_name:
-      ensure  => running,
       enable  => true,
+      active  => true,
     }
   }
 }
